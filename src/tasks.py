42b690f45b9cf71797223800543a3554295ffc93
dbsxodud-11 <99yty@kaist.ac.kr>
2023-03-20 19:14:52 +0900

    slight change

--- src/tasks.py
+++ src/tasks.py
@@ -19,9 +19,10 @@ class Sine_Task():
         Compute the true function on the given x.
         """
         
+        # return self.amplitude * np.sin(self.phase + x)
         return self.amplitude * np.sin(self.phase + x)
         
-    def sample_data(self, size=1):
+    def sample_data(self, num_points=1, device="cuda"):
         """
         Sample data from this task.
         
@@ -29,12 +30,13 @@ class Sine_Task():
             x: the feature vector of length size
             y: the target vector of length size
         """
+        batch_size = self.amplitude.shape[0]
         
-        x = np.random.uniform(self.xmin, self.xmax, size)
+        x = np.random.uniform(self.xmin, self.xmax, (batch_size, num_points))
         y = self.true_function(x)
         
-        x = torch.tensor(x, dtype=torch.float).unsqueeze(1)
-        y = torch.tensor(y, dtype=torch.float).unsqueeze(1)
+        x = torch.tensor(x, dtype=torch.float).unsqueeze(-1).to(device)
+        y = torch.tensor(y, dtype=torch.float).unsqueeze(-1).to(device)
         
         return x, y
 
@@ -51,13 +53,21 @@ class Sine_Task_Distribution():
         self.x_min = x_min
         self.x_max = x_max
         
-    def sample_task(self):
+    def sample_task(self, batch_size):
         """
         Sample from the task distribution.
         
         returns:
             Sine_Task object
         """
-        amplitude = np.random.uniform(self.amplitude_min, self.amplitude_max)
-        phase = np.random.uniform(self.phase_min, self.phase_max)
+        amplitude = np.random.uniform(self.amplitude_min, self.amplitude_max, size=(batch_size, 1))
+        phase = np.random.uniform(self.phase_min, self.phase_max, size=(batch_size, 1))
         return Sine_Task(amplitude, phase, self.x_min, self.x_max)
+
+
+if __name__ == "__main__":
+    task_distribution = Sine_Task_Distribution(amplitude_min=0.1, amplitude_max=0.5, phase_min=0, phase_max=2*np.pi,
+                                               x_min=-5.0, x_max=5.0)
+    tasks = task_distribution.sample_task(batch_size=16)
+    print(tasks.amplitude, tasks.phase)
+    x, y = tasks.sample_data(num_points=10)
